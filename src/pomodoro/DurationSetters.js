35234f7f5b04dcc17134d7f14b7ca2c5e9a7dde7
Derek Blauser <derek.blauser@gmail.com>
2021-07-24 13:19:58 -0400

    Finished creating all components and ensure they each have a single responsibility

---
+++ src/pomodoro/DurationSetters.js
@@ -0,0 +1,40 @@
+import React from "react";
+import FocusSetter from "./FocusSetter";
+import BreakSetter from "./BreakSetter";
+
+function DurationSetters({
+  focusDuration,
+  handleFocusDecrease,
+  session,
+  minFocus,
+  handleFocusIncrease,
+  maxFocus,
+  breakDuration,
+  handleBreakDecrease,
+  minBreak,
+  handleBreakIncrease,
+  maxBreak,
+}) {
+  return (
+    <div className="row">
+      <FocusSetter
+        focusDuration={focusDuration}
+        handleFocusDecrease={handleFocusDecrease}
+        session={session}
+        minFocus={minFocus}
+        handleFocusIncrease={handleFocusIncrease}
+        maxFocus={maxFocus}
+      />
+      <BreakSetter
+        breakDuration={breakDuration}
+        handleBreakDecrease={handleBreakDecrease}
+        session={session}
+        minBreak={minBreak}
+        handleBreakIncrease={handleBreakIncrease}
+        maxBreak={maxBreak}
+      />
+    </div>
+  );
+}
+
+export default DurationSetters;
